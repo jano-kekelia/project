3a26a306cf9dc20dfbc232b9870d431e36916b52
jano-kekelia <kjansughi@gmail.com>
2025-01-12 20:48:31 +0400

    commit

---
+++ level34/classwork/classwork2.py
@@ -0,0 +1,10 @@
+def sum_or_multiply(num1, num2):
+    if num1 > 50:
+        print(num1 * num2)
+    else:
+        print(num1 + num2)
+
+
+sum_or_multiply(70, 10)  
+sum_or_multiply(100, 10) 
+sum_or_multiply(58, 90)  

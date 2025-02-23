a6416212883db60fce19425810c580b9dd15b8d3
jano-kekelia <kjansughi@gmail.com>
2025-02-23 21:06:48 +0400

    commit

---
+++ level40/classwork/classwork.py
@@ -0,0 +1,7 @@
+def greet(a,b):
+    num1= a * b
+    return num1
+
+result = greet(c,d)
+double= result * 2
+print(double)
---
+++ level40/classwork/classwork2.py
@@ -0,0 +1,4 @@
+def greet(name="jano", surname="kekelia"):
+    print(f" {name} {surname}")
+print(greet("jano" "kekelia"))
+print(greet())
